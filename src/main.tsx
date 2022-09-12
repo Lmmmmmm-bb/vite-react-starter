23750470c8ae4f398042ac7445cc60405186fcca
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-09-13 00:05:44 +0800

    chore: clean up

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app';
+
 import './index.scss';
+import App from './app';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
