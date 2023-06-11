c8434090083821143695e3f367be365dfeed70b8
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-06-11 16:36:29 +0800

    chore: init uno

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './index.scss';
 import App from './app';
+
+import 'virtual:uno.css';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
