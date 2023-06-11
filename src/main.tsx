da51a406d5c8f8904087f62ac8d94024a883caf9
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-06-11 16:47:49 +0800

    feat(uno): css reset

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './app';
 
 import 'virtual:uno.css';
+import '@unocss/reset/tailwind-compat.css';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
