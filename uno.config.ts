9a4856bad2bb36ba32a2efca3bbd392717f90a45
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-09-10 00:08:56 +0800

    chore: font

--- uno.config.ts
+++ uno.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, presetAttributify, presetIcons, presetUno } from 'unocss';
+import { defineConfig, presetAttributify, presetIcons, presetUno, presetWebFonts } from 'unocss';
 import transformerAttributifyJsx from '@unocss/transformer-attributify-jsx';
 
 export default defineConfig({
@@ -7,6 +7,11 @@ export default defineConfig({
     presetIcons({
       warn: true,
       scale: 1.2,
+    }),
+    presetWebFonts({
+      fonts: {
+        sans: 'Inter',
+      },
     }),
     presetAttributify(),
   ],
