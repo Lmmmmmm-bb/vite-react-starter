ff3c1df9e9a139e71e7a2c6ba8c4ae54707216ba
_lmmmmmm <lmmmmmm12138@gmail.com>
2024-09-26 09:59:10 +0800

    chore: styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from './app';
 
-import './index.css';
+import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
